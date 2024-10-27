e52427f96db43df5d485ef0ac380e9b86d103526
Hozaifazaki <hozaifa.zaki.11@gmail.com>
2024-10-27 21:55:24 +0300

    Updated the code to work with haystack and llama.cpp

---
+++ src/data_models/prompts.py
@@ -0,0 +1,7 @@
+class Prompts:
+    SYSTEM_PROMPT = "You are a helpful assistant"
+
+    DEAFUALT_RESPONSE = "How can I help you!"
+
+    PROMPT_TEMPLATE = """<|system|>{system_prompt}<|end|><|user|>{user_prompt}<|end|><assistant>"""
+    
