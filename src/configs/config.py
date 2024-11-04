1f71d6e4a2b2855b7ab74c2e9c472bd11a62d58c
Hozaifazaki <hozaifa.zaki.11@gmail.com>
2024-11-05 00:22:33 +0200

    Updated the max_new_tokens

--- src/configs/config.py
+++ src/configs/config.py
@@ -1,10 +1,10 @@
 class Config:
     ONLINE_DOWNLOAD = True
-    BATCH_SIZE = 8
+    BATCH_SIZE = 1
 
-    END_TOKENS = ["<|endoftext|>", "<|end|>", "</s>", "<|assistant|>"]
+    # END_TOKENS = ["<|endoftext|>", "<|end|>", "</s>", "<|assistant|>"]
 
     GENERATION_ARGS: dict = {
-        "max_new_tokens": 256,
+        "max_new_tokens": 1024,
         "do_sample": False,
     }
